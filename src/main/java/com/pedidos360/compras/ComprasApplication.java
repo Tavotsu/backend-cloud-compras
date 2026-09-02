b5745dc5a10fef918d48ecc1198a71ea267dd018
Tavotsu <123342265+Tavotsu@users.noreply.github.com>
2026-09-02 10:47:18 -0400

    chore: inicializar proyectos base de compras con spring boot

---
+++ src/main/java/com/pedidos360/compras/ComprasApplication.java
@@ -0,0 +1,13 @@
+package com.pedidos360.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedidos360/compras/ComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidos360.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
